31692347de8cebc2f46b4b767023bf1a411dfbba
g-shafiee <33800821+g-shafiee@users.noreply.github.com>
2017-12-05 15:40:10 +0330

    Update Test.java

--- Test.java
+++ Test.java
@@ -18,7 +18,7 @@
         static class MyHandler implements HttpHandler {
             @Override
             public void handle(HttpExchange t) throws IOException {
-                String response = "This is my response at 13:28.";
+                String response = "This is my response at 15:39.";
                 t.sendResponseHeaders(200, response.length());
                 OutputStream os = t.getResponseBody();
                 os.write(response.getBytes());
